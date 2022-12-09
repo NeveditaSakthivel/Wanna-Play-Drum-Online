c5af85b9f5945c31ba357c21b4f37b6cd99baf81
NeveditaSakthivel <120033497+NeveditaSakthivel@users.noreply.github.com>
2022-12-09 21:00:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,31 +19,31 @@ function makesound(s)
   switch(s)
   {
     case 'W':
-    var crash = new Audio('./crash.mp3');
+    var crash = new Audio('crash.mp3');
     crash.play();
     break;
     case 'A':
-    var kick = new Audio('./kick-bass.mp3');
+    var kick = new Audio('kick-bass.mp3');
     kick.play();
     break;
     case 'S':
-    var snare = new Audio('./snare.mp3');
+    var snare = new Audio('snare.mp3');
     snare.play();
     break;
     case 'D':
-    var tom1 = new Audio('./tom-1.mp3');
+    var tom1 = new Audio('tom-1.mp3');
     tom1.play();
     break;
     case 'J':
-    var tom2 = new Audio('./tom-2.mp3');
+    var tom2 = new Audio('tom-2.mp3');
     tom2.play();
     break;
     case 'K':
-    var tom3 = new Audio('./tom-3.mp3');
+    var tom3 = new Audio('tom-3.mp3');
     tom3.play();
     break;
     case 'L':
-    var tom4 = new Audio('./tom-4.mp3');
+    var tom4 = new Audio('tom-4.mp3');
     tom4.play();
     break;
   }}
